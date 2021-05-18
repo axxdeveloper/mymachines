71d338c5a6320fb80006d8e62df023f5d40d9a20
Isaac Liao <shooeugenesea.tw@gmail.com>
2021-05-18 17:27:58 +0800

    fix flow

--- report.sh
+++ report.sh
@@ -1,8 +1,8 @@
 sleep 30 #wait for interfaces up
 HOST=`hostname`
+git pull --no-edit
 cd /home/isaac/projects/mymachines
 echo "`ip addr | grep inet`" > $HOST.txt
-git pull --no-edit
 git add .
 git commit -m 'change ip for '"$HOST"
 git push -f
