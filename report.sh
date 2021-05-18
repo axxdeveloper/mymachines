3205c882f49ab42c4d9f9f6fc0fba89334d5e866
Isaac Liao <shooeugenesea.tw@gmail.com>
2021-05-18 16:40:34 +0800

    change ip for isaac-ubuntu-dev

--- report.sh
+++ report.sh
@@ -2,7 +2,7 @@ sleep 30 #wait for interfaces up
 HOST=`hostname`
 cd /home/isaac/projects/mymachines
 echo "`ip addr | grep inet`" > $HOST.txt
-git pull
-git add .
-git commit -m 'change ip for '"$HOST"
-git push -f
+sh git pull
+sh git add .
+sh git commit -m 'change ip for '"$HOST"
+sh git push -f
